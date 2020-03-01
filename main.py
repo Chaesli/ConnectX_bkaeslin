2dfe2b47d591dc4e61302047a4bcacc01b0ff7aa
Chaesli <32617671+Chaesli@users.noreply.github.com>
2020-03-01 07:52:19 +0100

    Tried to finish the winning step finding process (horizontally and vertically). Found false understanding of the enumeration of the cells (see OneNote).

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 import scipy
 from random import choice
 
-
 from kaggle_environments import evaluate, make, utils
 
 env = make('connectx', debug=True)
@@ -19,6 +18,7 @@ def my_agent(observation, configuration):
     cfg_cols = configuration.columns
     cfg_inarow = configuration.inarow
     priorities = [3, 4, 2, 5, 1, 0, 6]
+
 
     def select_first(observation):
         for i in priorities:
@@ -41,15 +41,25 @@ def my_agent(observation, configuration):
 
         # Check horizontal possibility:
         for i in range(cfg_rows):
-            for j in range(cfg_cols - cfg_inarow -1):
-                if observation(j+cfg_cols*i) == player_num and observation(j+cfg_cols*i+1) == player_num and observation(j+cfg_cols*i+2) == player_num:
-                    if j != cfg_cols and observation(j+cfg_cols*i+3) == 0:
-                        return j+3
-                    elif j != 0 and observation(j+cfg_cols*i-1) == 0:
-                        return j-1
-                    else:
-                        return -99
+            for j in range(cfg_cols - cfg_inarow - 1):
+                if observation.board[j + cfg_cols * i] == player_num \
+                        and observation.board[j + cfg_cols * i + 1] == player_num \
+                        and observation.board[j + cfg_cols * i + 2] == player_num:
+                    if j != (cfg_cols-1)*(i+1) and observation.board[j + cfg_cols * i + 3] == 0:
+                        return j + 3
+                    elif j != cfg_cols*i and observation.board[j + cfg_cols * i - 1] == 0:
+                        return j - 1
 
+        # Check vertical possibility:
+        for i in range(cfg_cols):
+            for j in range(cfg_rows - cfg_inarow - 1):
+                if observation.board[i + cfg_rows * j] == player_num \
+                        and observation.board[i + cfg_rows * (j + 1)] == player_num \
+                        and observation.board[i + cfg_rows * (j + 2)] == player_num \
+                        and observation.board[i + cfg_rows * (j + 3)] == 0:
+                    return i
+        else:
+            return -99
 
     def round_number(observation):
         count_1 = 0
@@ -65,7 +75,7 @@ def my_agent(observation, configuration):
             start_player = 2
         else:
             start_player = 0
-        return count_1+count_2, start_player#, count_1, count_2
+        return count_1 + count_2, start_player  # , count_1, count_2
 
     r_num, start_player = round_number(observation=observation)
 
@@ -73,7 +83,21 @@ def my_agent(observation, configuration):
     if r_num < 2:
         return select_first(observation=observation)
     else:
-        return choice([c for c in range(configuration.columns) if observation.board[c] == 0])
+        # Check own winning possibility:
+        own_win = winning_next(observation=observation, player_num=1, cfg_rows=cfg_rows, cfg_cols=cfg_cols,
+                               cfg_inarow=cfg_inarow)
+        print('Own winning: {}'.format(own_win))
+        if own_win >= 0:
+            return own_win
+
+        # Check opposite winning possibility:
+        opposite_win = winning_next(observation=observation, player_num=2, cfg_rows=cfg_rows, cfg_cols=cfg_cols,
+                                    cfg_inarow=cfg_inarow)
+        print('Opposite winning: {}'.format(opposite_win))
+        if opposite_win >= 0:
+            return opposite_win
+        else:
+            return choice([c for c in range(configuration.columns) if observation.board[c] == 0])
 
 
 # Play as first position against random agent.
@@ -83,23 +107,22 @@ observation = trainer.reset()
 
 while not env.done:
     my_action = my_agent(observation, env.configuration)
-    #print("My Action", my_action)
+    # print("My Action", my_action)
     observation, reward, done, info = trainer.step(my_action)
     # env.render(mode="ipython", width=100, height=90, header=False, controls=False)
 env.render()
-
 
 def mean_reward(rewards):
     return sum(r[0] for r in rewards) / sum(r[0] + r[1] for r in rewards)
 
 
+'''
 # Run multiple episodes to estimate its performance.
 print("My Agent vs Random Agent:", mean_reward(evaluate("connectx",
                                                         [my_agent, "random"], num_episodes=10)))
 print("My Agent vs Negamax Agent:", mean_reward(evaluate("connectx",
                                                          [my_agent, "negamax"], num_episodes=10)))
 
-'''
 def write_agent_to_file(function, file):
     with open(file, "a" if os.path.exists(file) else "w") as f:
         f.write(inspect.getsource(function))
